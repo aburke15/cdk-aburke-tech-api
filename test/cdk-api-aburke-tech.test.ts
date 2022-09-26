76113208a97ffd2aa60d44145a3e109e3a657e9b
Andre Burke <aburke.dev@gmail.com>
2022-09-25 21:09:53 -0600

    Initial commit

---
+++ test/cdk-api-aburke-tech.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkApiAburkeTech from '../lib/cdk-api-aburke-tech-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-api-aburke-tech-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkApiAburkeTech.CdkApiAburkeTechStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});

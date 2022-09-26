3b718543c0475b44eeaa05eb95bac7a1e34c37da
Andre Burke <aburke.dev@gmail.com>
2022-09-25 23:46:29 -0600

    added some implementation details to both constructs

--- lib/cloud-resume-construct.ts
+++ lib/cloud-resume-construct.ts
@@ -1,7 +1,25 @@
+import { Duration } from 'aws-cdk-lib';
+import { Code, IFunction } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
 
 export class CloudResumeConstruct extends Construct {
+  // private members to be used across funcs in this class
+  private readonly memorySize: number = 128;
+  private readonly timeout: Duration = Duration.seconds(30);
+
+  // expose this function to be referenced outside of this class
+  public readonly PageVisitCountFunction: IFunction;
+
   constructor(scope: Construct, id: string) {
     super(scope, id);
+
+    const pageVisitCountFunction: IFunction = new NodejsFunction(this, 'PageVisitCountFunction', {
+      memorySize: this.memorySize,
+      timeout: this.timeout,
+      entry: Code.fromAsset('').path + '/page-visit-count-function.ts',
+    });
+
+    this.PageVisitCountFunction = pageVisitCountFunction;
   }
 }
--- lib/portfolio-construct.ts
+++ lib/portfolio-construct.ts
@@ -1,20 +1,28 @@
 import { Duration } from 'aws-cdk-lib';
-import { IFunction } from 'aws-cdk-lib/aws-lambda';
+import { Code, IFunction } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
-import { Construct } from 'constructs';
+import { Construct, Node } from 'constructs';
 
 export class PortfolioConstruct extends Construct {
+  // private members that can be shared across functions
+  private readonly memorySize: number = 256;
+  private readonly timeout: Duration = Duration.seconds(30);
+
+  // public exposed functions to be added to the api gateway
+  public readonly GetPortfolioProjectsFunction: IFunction;
+
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    const memorySize: number = 240;
-    const timeout: Duration = Duration.seconds(30);
-
-    const insertReposFunction: IFunction = new NodejsFunction(this, 'InsertReposFunction', {
-      memorySize: 240,
-      timeout: Duration.seconds(30),
+    const getPortfolioProjectsFunction: IFunction = new NodejsFunction(this, 'GetPortfolioProjectsFunction', {
+      memorySize: this.memorySize,
+      timeout: this.timeout,
+      entry: Code.fromAsset('').path + '/get-portfolio-projects-function.ts',
+      bundling: {
+        minify: true,
+      },
     });
 
-    const deleteReposFunction: IFunction = new NodejsFunction(this, 'DeleteReposFunction', {});
+    this.GetPortfolioProjectsFunction = getPortfolioProjectsFunction;
   }
 }

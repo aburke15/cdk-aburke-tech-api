bb49e2579c697ae64909afde1bdded74d38545ad
Andre Burke <aburke.dev@gmail.com>
2022-09-29 23:58:40 -0600

    added a types file

---
+++ lib/common/types.ts
@@ -0,0 +1,8 @@
+export interface GitHubProject {
+  id: string | undefined;
+  name: string | undefined;
+  createdAt: string | undefined;
+  description: string | undefined;
+  htmlUrl: string | undefined;
+  language: string | undefined;
+}
